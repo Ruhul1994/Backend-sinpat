9ffa828e45009deb327d3ba753af0cce0040849c
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2025-03-10 21:00:43 +0600

    refactor homepage and add new routes; create index.html for main page layout

--- express.js
+++ express.js
@@ -4,46 +4,15 @@ const port = 3000;
 
 
 app.get('/', (req, res)=>{
-    res.send(`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Express page</title>
-    <style>
-        *{
-        margin : 0;
-        padding : 0;
-        box-sizing: border-box;
-        }
-        body{
-            font-family: Arial, Helvetica, sans-serif;
-            font-size: 16px;
-        }
-        .container{
-            width: 50%;
-            margin: 2rem auto;
-            padding: 1rem;
-            background: #dddeee;
-            border-radius:.5em;
-        }
-    .btn {
-    padding: 1em;
-    margin: 1em;
-    cursor:pointer;
-    }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <h1>Express app </h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis fuga commodi dolor quisquam ipsam quaerat ratione neque molestias iste non porro architecto beatae, mollitia velit itaque ea deserunt optio dolorum.</p>
-        <Button class="btn">Count +</Button>
-        <Button class="btn">Add</Button>
-    </div>
-</body>
-</html>`);
+    res.send(`<h1>this is home page</h1>`);
 });
+app.get('/about', (req, res)=>{
+    res.send(`<h1>this is about page</h1>`)
+});
+
+app.get('/contact', (req, res)=>{
+    res.send('<h1>this is contact page</h1>')
+})
 
 app.listen(port,()=>{
     console.log(`the app is listening on port ${port}`);
